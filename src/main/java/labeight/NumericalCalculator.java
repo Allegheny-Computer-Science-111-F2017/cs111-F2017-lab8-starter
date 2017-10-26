a82149d72170bf49f2a75e6850c1e1415ae3f029
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2017-10-26 09:19:23 -0400

    Adding the first version of the NumericalCalculator.

---
+++ src/main/java/labeight/NumericalCalculator.java
@@ -0,0 +1,67 @@
+package labeight;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+/**
+ * This class calculates different numerical values for a list of numbers.
+ *
+ * <p>Bugs: This program has no known bugs, but is missing source code
+ *
+ * @author Gregory M. Kapfhammer
+ */
+public class NumericalCalculator {
+
+  /**
+   * Count the number of items in a list of numbers.
+   *
+   * @author Gregory M. Kapfhammer
+   */
+  public static int countNumbersInList(ArrayList<Integer> numberList) {
+    // TODO: Add an implementation for this method
+  }
+
+  /**
+   * Display an entire list provided as an argument.
+   *
+   * @author Gregory M. Kapfhammer
+   */
+  public static void displayNumberList(ArrayList<Integer> numberList) {
+    // TODO: Add an implementation for this method
+  }
+
+  /**
+   * Finds the minimum value in a list of values.
+   *
+   * @author Gregory M. Kapfhammer
+   */
+  public static int findMinimumValue(ArrayList<Integer> numberList) {
+    // TODO: Add an implementation for this method
+  }
+
+  /**
+   * Finds the maximum value in a list of values.
+   *
+   * @author Gregory M. Kapfhammer
+   */
+  public static int findMaximumValue(ArrayList<Integer> numberList) {
+    // TODO: Add an implementation for this method
+  }
+
+  /**
+   * Calculate the arithmetic mean of a list of values.
+   *
+   * @author Gregory M. Kapfhammer
+   */
+  public static double calculateArithmeticMean(ArrayList<Integer> numberList) {
+    // TODO: Finish the computations in this method
+    double arithmeticMean = 0.0;
+    Iterator<Integer> numberListIterator = numberList.iterator();
+    while (numberListIterator.hasNext()) {
+
+    }
+
+    return arithmeticMean;
+  }
+
+}
